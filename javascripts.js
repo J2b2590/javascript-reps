853a7cd6866949c1f18192d3bdfcd4453dfd3586
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2017-09-14 16:28:05 -0500

    so close but couldnt figure out the concatination

---
+++ javascripts.js
@@ -0,0 +1,80 @@
+// declare function named "lengths"
+// that accepts a arguments named "arrayOfStrings"
+constlengths = (arrayOfStrings) => {
+
+  // we can log out our "arrayOfStrings"
+  console.log(arrayOfStrings);
+
+  // now, we want to "return" something... but what?
+
+  return whateverVariableYouWantToReturnHere;
+
+}
+//round 1
+
+const words = ['green', 'blue', 'bob', 'glob']
+
+const lengths = (arrayOfStrings) =>{
+	
+	const numberString = arrayOfStrings
+
+for (let i = 0; i < numberString.length; i++){
+	numberString[i] = arrayOfStrings[i].length 
+		};
+		return numberString
+};
+
+
+//Round 2
+const transmogrifier = (x,y,z) =>{
+const sup = Math.pow((x * y), z);
+return sup
+}
+transmogrifier(5, 4, 3)
+
+
+//Round 3
+
+const toonify = (accent, sentence) =>{
+	let accent = 
+}
+
+
+
+//Round 4
+
+const stringWord = 'hello'
+
+const wordReverse = (backwards) =>{
+	//creating a new const with a broken up string in an array
+	const wordInString = backwards.split(''); 
+	console.log(wordInString, ' this is wordInString variable')
+	// backwars.split() = wordInString.reverse();
+	
+}
+
+
+
+//round 5
+const letterReverse = (someString) =>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
